85a790f33e1941ab5dc6a2f21b019318a49bf4b8
jorj-kare <jorj.kare@gmail.com>
2022-09-28 16:59:08 +0200

    refactor: logout functionality

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,9 +5,10 @@ const logout = async () => {
     const url = `${window.location.origin}/api/v1/users/logout`;
     const res = await fetch(url);
     if (res.ok) {
+      showAlert("You have successfully logged out", "success", 1);
       window.setTimeout(() => {
         location.assign("/");
-      }, 500);
+      }, 1000);
     }
   } catch (err) {
     showAlert("Something went wrong, please try again!", "error", 5);
